5969ac9cb750201ca7720dd0a420e28a2ce3db9d
01192mg <01192mg@gmail.com>
2023-02-14 02:22:16 +0900

    feat: Cache-Aside 적용
    userProfile을 조회할 때 userName에 Cache-Aside 적용해서 호출 시간을 체크

---
+++ src/main/java/com/example/redis/service/ExternalApiService.java
@@ -0,0 +1,44 @@
+package com.example.redis.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class ExternalApiService {
+    public String getUserName(String userId) {
+        log.info("[getUserName] userId={}", userId);
+
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        if (userId.equals("A")) {
+            return "Adam";
+        }
+        if (userId.equals("B")) {
+            return "Bob";
+        }
+        return "";
+    }
+
+    public int getUserAge(String userId) {
+        log.info("[getUserAge] userId={}", userId);
+
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        if (userId.equals("A")) {
+            return 20;
+        }
+        if (userId.equals("B")) {
+            return 30;
+        }
+        return 0;
+    }
+}
